6bbc054858079b5ef807647250e644b0cfd23b42
Truong Dang <tdang@justworks.com>
2024-07-08 11:26:56 -0400

    finish values, types, dec+bin+hex

--- 018-printf-decimal-hex/main.go
+++ 018-printf-decimal-hex/main.go
@@ -11,10 +11,14 @@ func main() {
 
 	// print these values as both binary & hexadecimal
 	a, b, c, d, e, f := 0, 1, 2, 3, 4, 5
-	fmt.Printf("%v \t %b \t %#X\n",a,a,a)
-	fmt.Printf("%v \t %b \t %#X\n",b,b,b)
-	fmt.Printf("%v \t %b \t %#X\n",c,c,c)
-	fmt.Printf("%v \t %b \t %#X\n",d,d,d)
-	fmt.Printf("%v \t %b \t %#X\n",e,e,e)
-	fmt.Printf("%v \t %b \t %#X\n",f,f,f)
+	fmt.Printf("%v \t %b \t %#X\n", a, a, a)
+	fmt.Printf("%v \t %b \t %#X\n", b, b, b)
+	fmt.Printf("%v \t %b \t %#X\n", c, c, c)
+	fmt.Printf("%v \t %b \t %#X\n", d, d, d)
+	fmt.Printf("%v \t %b \t %#X\n", e, e, e)
+	fmt.Printf("%v \t %b \t %#X\n", f, f, f) // 5   101   0X5
+
+	eva := -1
+	fmt.Printf("-1 as binary, %b \n", eva)      // still -1
+	fmt.Printf("-1 as hexadecimal, %x \n", eva) // still -1
 }
--- 020-val-type-conversion-short-dec/main.go
+++ 020-val-type-conversion-short-dec/main.go
@@ -9,11 +9,11 @@ var w = 99
 func main() {
 	y := 42
 	z := 42.0
-	fmt.Printf("%v of type %T \n", y, y)
-	fmt.Printf("%v of type %T \n", z, z)
+	fmt.Printf("%v of type %T \n", y, y) // 42, int
+	fmt.Printf("%v of type %T \n", z, z) // 42, float64
 
 	var m float32 = 43.742
-	fmt.Printf("%v of type %T \n", m, m)
+	fmt.Printf("%v of type %T \n", m, m) // float32 because declared
 
 	/*
 		// this does not work!
@@ -21,9 +21,9 @@ func main() {
 		fmt.Printf("%v of type %T \n", z, z)
 	*/
 
-	/*
-		// this does work
-		z = float64(m)
-		fmt.Printf("%v of type %T \n", z, z)
-	*/
+	// this does work
+	z = float64(m)
+	fmt.Printf("%v of type %T \n", z, z)
 }
+
+// %T --> type
